95f9380b65f128a3496d5d1216e84d39b86d0705
Fellipe Lorram <fellipelorram@gmail.com>
2023-04-17 17:57:18 -0300

    Feat: Adding Contact Modal

--- src/components/layout/get-to-know-me/contact/contact.tsx
+++ src/components/layout/get-to-know-me/contact/contact.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Wrapper } from '../../wrapper'
 import Button from '@/components/button/button'
+import { ContactModal } from './contactModal'
 
 export function Contact() {
+    const [onScreen, setOnScreen] = useState(false);
+
     return (
         <Wrapper
             className='py-20 px-10'
@@ -10,12 +13,19 @@ export function Contact() {
                 background: 'linear-gradient(35deg,rgb(6,0,151) 0%,rgb(130,4,255) 73%,rgb(193,15,255) 100%)'
             }}
         >
+            <ContactModal
+                onScreen={onScreen}
+                setOnScreen={setOnScreen}
+            />
             <div className='w-full text-center'>
                 <h1 className='text-4xl font-semibold font-jakarta md:text-6xl'>
-                    Get a professional <br/> website today!
+                    Get a professional <br /> website today!
                 </h1>
 
-                <Button className='mt-12'>
+                <Button
+                    onClick={() => setOnScreen(true)}
+                    className='mt-12'
+                >
                     Contact Us
                 </Button>
             </div>
---
+++ src/components/layout/get-to-know-me/contact/contactModal.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const backDrop = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+    exit: { opacity: 0 },
+};
+
+const modalVariants = {
+    hidden: { scale: 0 },
+    visible: {
+        x: 0,
+        scale: 1,
+        transition: {
+            duration: 0.2,
+            delay: 0.2,
+        },
+    },
+    exit: { scale: 0 },
+};
+
+interface Props {
+    onScreen: boolean;
+    setOnScreen: (value: boolean) => void;
+}
+
+export function ContactModal({ onScreen, setOnScreen }: Props) {
+    return (
+        <AnimatePresence>
+            {onScreen && (
+                <motion.div
+                    onClick={() => setOnScreen(false)}
+                    variants={backDrop}
+                    className='bg-violet-900/50 w-full flex items-center justify-center fixed h-screen top-0 left-0 z-50'
+                    initial='hidden'
+                    animate='visible'
+                    exit='exit'
+
+                >
+                    <motion.div
+                        variants={modalVariants}
+                        className='w-full max-w-2xl bg-white-100 rounded-lg shadow-lg p-10 '
+                    >
+                        <h1 className='text-2xl font-semibold font-jakarta text-gray48 text-center'>
+                            Get a professional website today!
+                        </h1>
+                         
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>
+    )
+}
